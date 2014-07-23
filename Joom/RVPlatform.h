88273d90af3b61d42a485861facb04ab1d4ecc1f
Rohit Verma <rohitverma@live.ca>
2014-07-22 23:58:31 -0400

    Adding object oriented platforms

---
+++ Joom/RVPlatform.h
@@ -0,0 +1,13 @@
+//
+//  RVPlatform.h
+//  Joom
+//
+//  Created by Rohit Verma on 2014-07-22.
+//  Copyright (c) 2014 rohitv. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface RVPlatform : SKSpriteNode
+
+@end
---
+++ Joom/RVPlatform.m
@@ -0,0 +1,13 @@
+//
+//  RVPlatform.m
+//  Joom
+//
+//  Created by Rohit Verma on 2014-07-22.
+//  Copyright (c) 2014 rohitv. All rights reserved.
+//
+
+#import "RVPlatform.h"
+
+@implementation RVPlatform
+
+@end
