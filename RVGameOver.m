2a6af111012a44a2c9f723099152bbfe83f0d67a
Rohit Verma <rohitverma@live.ca>
2014-08-07 22:42:45 -0400

    Changing font size to fit 3.5inch screens

--- RVGameOver.m
+++ RVGameOver.m
@@ -35,7 +35,7 @@
         score.position = CGPointMake(size.width/2, size.height-100);
        
         SKLabelNode *nextLevel = [SKLabelNode labelNodeWithFontNamed:@"AppleSDGothicNeo-Regular"];
-        nextLevel.fontSize = 20;
+        nextLevel.fontSize = 18;
         nextLevel.text = [NSString stringWithFormat:@"Total Score: %i | Points to Next Health Upgrade: %i", totalScore, (200-(totalScore % 200))];
         nextLevel.position = CGPointMake(size.width/2, size.height-140);
         
